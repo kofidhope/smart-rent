07520d4ec5d1f0f171135a2a7446239bd7ff322c
Kofidhope <forsonbentum55@gmail.com>
2026-05-04 12:53:32 +0000

    payment-service updated

--- payment-service/src/main/java/com/kofi/paymentservice/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/kofi/paymentservice/PaymentServiceApplication.java
@@ -2,8 +2,16 @@ package com.kofi.paymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableKafka
+@EnableScheduling
 public class PaymentServiceApplication {
 
     public static void main(String[] args) {
