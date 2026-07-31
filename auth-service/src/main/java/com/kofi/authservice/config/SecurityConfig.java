e25db78c5966fda686b15cdd058a6a0b9c5734d1
Kofidhope <forsonbentum55@gmail.com>
2026-07-31 20:00:27 +0000

    auth-service updated

--- auth-service/src/main/java/com/kofi/authservice/config/SecurityConfig.java
+++ auth-service/src/main/java/com/kofi/authservice/config/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("api/auth/**").permitAll()
                         .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated()
                 );
